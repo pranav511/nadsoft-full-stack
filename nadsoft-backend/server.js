5184ce242b3855a57e04e89a13dad2ac02e59d58
Pranav <88698969+pranav511@users.noreply.github.com>
2026-07-01 01:14:45 +0530

    change server.js file to check

--- nadsoft-backend/server.js
+++ nadsoft-backend/server.js
@@ -14,5 +14,5 @@ app.use(express.json());
 app.use("/students", studentRoutes);
 
 app.listen(PORT,()=>{
- console.log(`Server lives on port ${PORT}`);
+ console.log(`Server lives at ${PORT}`);
 });
